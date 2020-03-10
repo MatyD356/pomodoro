17561fdfac8a98b1db265c88e120fc205dbdeeb9
Mateusz <matyd356@gmail.com>
2020-03-10 15:24:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sesionsCount: 0,
+      sessionTime: 25,
+      breakTime: 5
+    }
+  }
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+    document.body.appendChild(script);
+  }
+  render() {
+    return (
+      <div className="App" />
+    );
+  }
 }
 
 export default App;
