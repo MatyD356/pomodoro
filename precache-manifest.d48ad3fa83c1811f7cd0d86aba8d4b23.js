3e807dd005d9d174e469902d297d666617ee5b5f
Mateusz <matyd356@gmail.com>
2020-03-12 09:26:46 +0100

    Updates

---
+++ precache-manifest.d48ad3fa83c1811f7cd0d86aba8d4b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd942e8a80a9e7c8da13734c08a41c0e",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "1b8d31526685ba00e512",
+    "url": "/pomodoro/static/css/main.29342a1c.chunk.css"
+  },
+  {
+    "revision": "145502e9ca5c501b102b",
+    "url": "/pomodoro/static/js/2.0bb7294c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pomodoro/static/js/2.0bb7294c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8d31526685ba00e512",
+    "url": "/pomodoro/static/js/main.d5d38837.chunk.js"
+  },
+  {
+    "revision": "7f95e87765d4b9c823c5",
+    "url": "/pomodoro/static/js/runtime-main.ba2efca2.js"
+  }
+]);
